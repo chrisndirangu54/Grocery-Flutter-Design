f6b5f4af56fd3b7e03c5689e0a20014976bdee1a
chrisndirangu54 <chrisspringsten@gmail.com>
2024-09-02 12:48:51 +0300

    VER

--- .firebase/sokoni-s-grocery/hosting/flutter_bootstrap.js
+++ .firebase/sokoni-s-grocery/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1146946052"
+    serviceWorkerVersion: "2593809738"
   }
 });
--- .firebase/sokoni-s-grocery/hosting/flutter_service_worker.js
+++ .firebase/sokoni-s-grocery/hosting/flutter_service_worker.js
@@ -53,14 +53,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f2ff002ea5375d41ea4b4ff984affc17
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d43f3cb1fa363d3b4bd721e32f941f82",
+"flutter_bootstrap.js": "cc51ce7e8aea1e1df47fabdd9e0d75e5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b57b27b6bb35398c69e0efcfbfaba6e9",
 "/": "b57b27b6bb35398c69e0efcfbfaba6e9",
-"main.dart.js": "4b207a6e281937c22ba6247ab011aecb",
+"main.dart.js": "f63e2f79c614a2a15ba88f1b726a0fa1",
 "manifest.json": "b9d15d0ede9adf54f1c5c449a548e618",
 "version.json": "c55588c7a53cf82a4a557da5ee1a89c3"};
 // The application shell files that are downloaded before a service worker can
